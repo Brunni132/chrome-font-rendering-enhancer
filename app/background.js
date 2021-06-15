fb58b54519c7521386b62d225a8e4378e803c565
Florian <f_bron@hotmail.com>
2021-06-15 10:56:01 +0900

    New version

--- app/background.js
+++
@@ -1,7 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  if (chrome.runtime.openOptionsPage) {
-    chrome.runtime.openOptionsPage();
-  } else {
-    window.open(chrome.runtime.getURL('options.html'));
-  }
-});
